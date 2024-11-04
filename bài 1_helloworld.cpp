903fa59547040ca83e73d909d1804d7f3c4f9a93
nguyenbaoduy16022009@gmail.com <MAY 06@LAB2-18>
2024-11-04 11:24:34 +0700

    Add project files.

---
+++ bài 1_helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "hello world!\n";
+	cout << "toi ten la" << "nguyen bao duy" << endl;
+	cout << "toi dang choi c++" << endl;
+	return 0;
+}
+	   
